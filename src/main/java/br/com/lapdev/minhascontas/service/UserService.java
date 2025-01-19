68ca37a49773164bffdcfb4edb6da65d840b5df8
Lerry Augusto <lerry.pirani@gmail.com>
2025-01-19 20:38:59 -0300

    repository and service for user created

---
+++ src/main/java/br/com/lapdev/minhascontas/service/UserService.java
@@ -0,0 +1,46 @@
+package br.com.lapdev.minhascontas.service;
+
+import br.com.lapdev.minhascontas.dto.UserDTO;
+import br.com.lapdev.minhascontas.entity.UserEntity;
+import br.com.lapdev.minhascontas.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> getAll(){
+        List<UserEntity> users = userRepository.findAll();
+        return  users.stream().map(UserDTO::new).toList();
+    }
+
+    public void insert(UserDTO user){
+        UserEntity userEntity = new UserEntity(user);
+        userRepository.save(userEntity);
+    }
+
+    public UserDTO update(UserDTO user){
+        UserEntity userEntity = new UserEntity(user);
+        return new UserDTO(userRepository.save(userEntity));
+    }
+
+    public void delete(Long id){
+        Optional<UserEntity> user = userRepository.findById(id);
+        user.ifPresent(userEntity -> userRepository.delete(userEntity));
+    }
+
+    public UserDTO findById(Long id){
+        Optional<UserEntity> user = userRepository.findById(id);
+       if(user.isPresent()){
+           return  new UserDTO(userRepository.findById(id).orElse(null));
+       }
+       return null;
+
+    }
+
+}
