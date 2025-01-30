1ed370accf2ffc6347f43926e25ac614a4930884
Lerry Augusto <lerry.pirani@gmail.com>
2025-01-29 22:25:33 -0300

    jwt and spring security added to the project

---
+++ src/main/java/br/com/lapdev/minhascontas/security/jwt/JwtUtils.java
@@ -0,0 +1,56 @@
+package br.com.lapdev.minhascontas.security.jwt;
+
+import br.com.lapdev.minhascontas.service.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${project.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${project.jweExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateTokenFromUserDetailsImpl(UserDetailsImpl userDetails){
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(getSigninKey(), SignatureAlgorithm.ES512).compact();
+    }
+
+    public Key getSigninKey(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsernameToken(String token) {
+        return Jwts.parser().setSigningKey(getSigninKey()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(getSigninKey()).build().parseClaimsJws(authToken);
+            return true;
+        }catch(MalformedJwtException e) {
+            System.out.println("Token inválido " + e.getMessage());
+        }catch(ExpiredJwtException e) {
+            System.out.println("Token expirado " + e.getMessage());
+        }catch(UnsupportedJwtException e) {
+            System.out.println("Token não suportado " + e.getMessage());
+        }catch(IllegalArgumentException e) {
+            System.out.println("Token Argumento inválido " + e.getMessage());
+        }
+
+        return false;
+    }
+}
