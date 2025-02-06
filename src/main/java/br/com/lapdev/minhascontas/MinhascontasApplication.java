c53e2dc920da38f8f5777107cfc0b63f71ba78ea
Lerry Augusto <lerry.pirani@gmail.com>
2025-02-05 22:34:18 -0300

    config updates

--- src/main/java/br/com/lapdev/minhascontas/MinhascontasApplication.java
+++ src/main/java/br/com/lapdev/minhascontas/MinhascontasApplication.java
@@ -2,8 +2,10 @@ package br.com.lapdev.minhascontas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class MinhascontasApplication {
 
 	public static void main(String[] args) {
